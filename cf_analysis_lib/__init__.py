6c97dc541c400c1e4c98956e2b5ee89761127ec1
linsalrob <raedwards@gmail.com>
2024-12-11 16:44:19 +1030

    Encoded metadata into numerics

--- cf_analysis_lib/__init__.py
+++ cf_analysis_lib/__init__.py
@@ -4,8 +4,8 @@ A library of functions for analyzing the output of the CF analysis pipeline.
 
 from .read_data import read_taxonomy, read_metadata, corrections, pathogens, read_subsystems, sorted_presence_absence
 from .metadata_data import metadata_definitions
-from .clean_data import compatible_columns
+from .clean_data import compatible_columns, categories_to_numeric
 
 __all__ = ['read_taxonomy', 'read_metadata', 'corrections', 'pathogens', 'read_subsystems',
-           'sorted_presence_absence', 'compatible_columns', 'metadata_definitions']
+           'sorted_presence_absence', 'compatible_columns', 'categories_to_numeric', 'metadata_definitions']
 
--- cf_analysis_lib/clean_data.py
+++ cf_analysis_lib/clean_data.py
@@ -2,6 +2,7 @@
 Create some clean and encode data functions
 """
 import re
+import pandas as pd
 
 
 def compatible_columns(df):
@@ -18,3 +19,33 @@ def compatible_columns(df):
            "9": "nine"}
     df.columns = [re.sub(r'^(\d)', lambda x: nos[x.group(1)] + "_", c) for c in df.columns]
     return df
+
+def categories_to_numeric(df, sequence_type):
+    """
+    Convert categories and objects to numeric forms
+    We use numbers if we have them, we keep the MGI/MinION ID so we can use it to join tables, but we don't want to use it as a predictor.
+    We convert categories to their codes so we get numbers.
+    We drop the other columns so we don't try and predict on free text columns.
+    I'm not sure what to do about dates, yet, so I ignore them for now
+    :param df: The data frame to clean
+    :param sequence_type: The name of the column that is the sequence type
+    :return: A clean data frame
+    """
+
+    # we make a copy of each data frame and then overwrite the columns. Internally this is more efficient than making a df one
+    # column at a time.
+    encoded = df.copy()
+    to_delete = ['Pseudomonas_Culture']  # this is a duplicate columns with 'CS_Pseudomonas_aeruginosa'
+    for col in df.columns:
+        if pd.api.types.is_numeric_dtype(df[col]):
+            continue
+        elif col == sequence_type:
+            continue
+        elif df[col].dtypes == 'category':
+            encoded[col] = df[col].cat.codes
+        else:
+            print(f'Dropping {col}: {df[col].dtypes}')
+            to_delete.append(col)
+
+    encoded = encoded.drop(columns=to_delete)
+    return encoded
