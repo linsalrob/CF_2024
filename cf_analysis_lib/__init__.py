939eb162e96e842ea8ad42fa35c57f34bfabd525
linsalrob <raedwards@gmail.com>
2024-12-14 12:19:15 +1030

    adding bacterial pathogens as a function

--- cf_analysis_lib/__init__.py
+++ cf_analysis_lib/__init__.py
@@ -5,8 +5,10 @@ A library of functions for analyzing the output of the CF analysis pipeline.
 from .read_data import read_taxonomy, read_metadata, corrections, pathogens, read_subsystems, sorted_presence_absence
 from .metadata_data import metadata_definitions
 from .clean_data import compatible_columns, categories_to_numeric, remove_highly_correlated_data
+from .pathogens import BacterialPathogens 
 
 __all__ = ['read_taxonomy', 'read_metadata', 'corrections', 'pathogens', 'read_subsystems',
            'sorted_presence_absence', 'compatible_columns', 'categories_to_numeric', 'remove_highly_correlated_data',
-           'metadata_definitions']
+           'metadata_definitions',
+          'BacterialPathogens']
 
