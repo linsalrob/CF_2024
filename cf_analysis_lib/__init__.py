34ca6bec6bb3f6b269b8f6520cbeea91217604b6
linsalrob <raedwards@gmail.com>
2025-02-04 16:43:58 +1030

    create an msword doc from a pandas df

--- cf_analysis_lib/__init__.py
+++ cf_analysis_lib/__init__.py
@@ -8,10 +8,13 @@ from .clean_data import compatible_columns, categories_to_numeric, remove_highly
 from .pathogens import BacterialPathogens
 from .random_forests import random_forest_classifier, random_forest_regression, gb_classifier, gb_regressor
 from .plot_figures import plot_pca, plot_feature_importance, plot_feature_abundance, plot_top_features, plot_abundance_stripplot
+from .export_to_docx import pd2docx
 
 __all__ = ['read_taxonomy', 'read_metadata', 'corrections', 'read_subsystems',
            'sorted_presence_absence', 'compatible_columns', 'categories_to_numeric', 'remove_highly_correlated_data',
            'metadata_definitions', 'read_the_data',
            'BacterialPathogens',
            'random_forest_classifier', 'random_forest_regression', 'gb_classifier', 'gb_regressor', 'plot_top_features',
-           'plot_pca', 'plot_feature_importance', 'plot_feature_abundance', 'plot_top_features', 'plot_abundance_stripplot']
+           'plot_pca', 'plot_feature_importance', 'plot_feature_abundance', 'plot_top_features', 'plot_abundance_stripplot',
+           'pd2docx'
+        ]
