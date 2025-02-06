02be55bd6ced5316550ae452449842aa4615f8b8
linsalrob <raedwards@gmail.com>
2025-02-07 07:46:04 +1030

    adding a central library resource

---
+++ cf_analysis_lib/load_libraries.py
@@ -0,0 +1,60 @@
+"""
+A library of functions for analyzing the output of the CF analysis pipeline.
+"""
+
+
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+import matplotlib.colors as mcolors
+import matplotlib.dates as mdates
+from matplotlib.colors import ListedColormap
+import pandas as pd
+import seaborn as sns
+import json
+import random
+
+from itertools import cycle
+from collections import Counter
+
+import networkx as nx
+import community
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder, OrdinalEncoder
+from sklearn.inspection import permutation_importance
+from sklearn.impute import SimpleImputer
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier, GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.multioutput import MultiOutputClassifier, MultiOutputRegressor, ClassifierChain
+from sklearn.metrics import mean_squared_error, roc_curve, auc
+
+from scipy.stats import linregress
+from scipy.cluster.hierarchy import linkage, fcluster
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+
+# there is a FutureWarning in sklearn StandardScalar which is really annoying. This ignores it.
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+try:
+  import google.colab
+  IN_COLAB = True
+  from google.colab import drive
+  drive.mount('/content/drive')
+  datadir = '/content/drive/MyDrive/Projects/CF/Adelaide/CF_Data_Analysis'
+except ImportError:
+  IN_COLAB = False
+  datadir = '..'
+
+
+
+from adjustText import adjust_text
+
+
